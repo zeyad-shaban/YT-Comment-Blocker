d17419cc0473da636588fa6f20513eea5a1c2720
Zeyad Shaban <64459068+zeyadShapan@users.noreply.github.com>
2020-12-27 10:48:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// made by Code With Zeyad Youtube channel
+// https://www.youtube.com/channel/UCFjcc0rq2guK6kJ4va49zgA/featured
+function run() {
+    let commentsContainer = document.getElementById('comments');
+    commentsContainer.remove();
+}
+
+window.onload = run
